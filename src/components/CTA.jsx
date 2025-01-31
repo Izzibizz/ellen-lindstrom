fbd59617fc09c2fd7ea0f4cd328d50e7087b51ad
izzibizz <contact@izabellind.com>
2025-01-31 16:04:29 +0100

    hover

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,8 +3,8 @@ import { MdMailOutline } from "react-icons/md";
 export const CTA = () => {
   return (
     <>
-    <div className="fixed bottom-[30%] right-[-55px] rotate-270 laptop:rotate-0 laptop:right-8 laptop:bottom-8 bg-red z-40 p-2 px-4 text-sm text-warm-white laptop:rounded hover:scale-110 cursor-pointer">
-    <a href="mailto:ellen_lindstrom@hotmail.com" aria-label="Email me at ellen_lindstrom@hotmail.com" className="flex gap-2 items-center" >Kontakta mig <MdMailOutline className="laptop:w-6 laptop:h-6"/></a>
+    <div className="fixed bottom-[30%] right-[-55px] rotate-270 laptop:rotate-0 laptop:right-0 laptop:bottom-8 bg-red/80 z-40 p-4 px-6 text-sm text-warm-white rounded-t-lg laptop:rounded-l-lg laptop:rounded-r-none hover:scale-110 laptop:origin-right cursor-pointer">
+    <a href="mailto:ellen_lindstrom@hotmail.com" aria-label="Email me at ellen_lindstrom@hotmail.com" className="flex gap-2 items-center" >Kontakta mig <MdMailOutline className="rotate-90 laptop:rotate-0 laptop:w-6 laptop:h-6"/></a>
     </div>
     </>
   )
