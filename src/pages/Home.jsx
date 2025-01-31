89f4e4307d98a5e3c2299e75d4914c06bb156210
izzibizz <contact@izabellind.com>
2025-01-31 16:11:49 +0100

    took of useref

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,17 @@
 
-import { useRef } from "react";
+/* import { useRef } from "react"; */
 import { usePortfolioStore } from "../stores/usePortfolioStore"
 import { Loading } from "../components/Loading"
 import { Hero } from "../components/Hero"
 import { OmEllen } from "../components/OmEllen"
 import { CTA } from "../components/CTA"
 import { SwiperComp } from "../components/SwiperComp"
-import { IoIosArrowDropdown } from "react-icons/io";
 
 
 export const Home = () => {
   const { loading } = usePortfolioStore()
 
-  const omEllenRef = useRef(null);
+/*   const omEllenRef = useRef(null);
 
   const scrollToOmEllen = () => {
     if (omEllenRef.current) {
@@ -23,7 +22,7 @@ export const Home = () => {
         behavior: "smooth",
       });
     }
-  };
+  }; */
 
   return (
     <section className="min-h-screen mb-20 w-screen max-w-screen relative flex flex-col font-body gap-6">
@@ -33,13 +32,7 @@ export const Home = () => {
         <>
           <Hero />
           <SwiperComp />
-          <IoIosArrowDropdown
-            className="self-center text-stone-700 w-8 h-8 laptop:hidden cursor-pointer"
-            onClick={scrollToOmEllen}
-          />
-          <div ref={omEllenRef}>
             <OmEllen />
-          </div>
           <CTA />
         </>
       )}
