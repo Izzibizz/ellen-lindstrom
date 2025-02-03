3edb4232e68a937b125f8a472e38466698ad43ab
izzibizz <contact@izabellind.com>
2025-02-03 09:31:19 +0100

    new img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,13 +23,13 @@ export const Hero = () => {
   return (
     <div className="h-[70vh] ">
           <img
-      src={isLaptop ? "https://res.cloudinary.com/dbf8xygxz/image/upload/v1738250531/Ellen-Lindstrom-musikalartist-oscarsrevyn-rosa-2-white.desk_ef9az1.jpg" : "https://res.cloudinary.com/dbf8xygxz/image/upload/v1738251277/Ellen-Lindstrom-musikalartist-oscarsrevyn-rosa-2-white-mobile_rdma6g.jpg"}
+      src={isLaptop ? "https://res.cloudinary.com/dbf8xygxz/image/upload/v1738571340/ellen-lindstrom-musikalartist-2_qlgwlw.jpg" : "https://res.cloudinary.com/dbf8xygxz/image/upload/v1738571407/ellen-lindstrom-musikalartist-1_ttlxwr.jpg"}
       alt="Ellen Lindstrom musikal artist"
-      className={` z-0 opacity-30 absolute w-screen max-w-full top-0 left-0 h-[80vh] max-h-screen object-cover animate-zoomInOut`}
+      className={` z-0  absolute w-screen max-w-full top-0 left-0 h-[80vh] max-h-screen object-cover animate-zoomInOut`}
     />
-    <div className="absolute z-30 top-[5%] left-6 tablet:left-[10%] laptop:top-[10%] laptop:left-[10%]">
+    <div className="absolute z-30 top-[7%] left-6 tablet:left-[10%] laptop:top-[10%] laptop:left-[10%]">
     <img src={logo} alt="logo Ellen Lindstrom name" className="h-[80px] tablet:h-[130px] laptop:h-[200px] "/>
-    <h3 className="font-free text-red text-2xl tablet:text-3xl ml-10 laptop:ml-20">Musikalartist</h3>
+    {/* <h3 className="font-free text-red text-2xl tablet:text-3xl ml-10 laptop:ml-20">Musikalartist</h3> */}
     </div>
 
     </div>
