5b6ef15990b6a34a6d26792782ad7e52f7c42a2d
izzibizz <contact@izabellind.com>
2025-02-04 16:25:12 +0100

    galleri

--- src/components/Aktuellt.jsx
+++ src/components/Aktuellt.jsx
@@ -1,7 +1,7 @@
 export const Aktuellt = () => {
   return (
-    <section className="flex flex-col w-full bg-gradient-to-t from-red-600/100 to-red-400/80 py-16 gap-10 items-center laptop:items-start text-warm-white">
-      <div className="flex flex-col w-10/12 mx-auto laptop:w-8/12 gap-4 ">
+    <section className="flex flex-col w-full bg-gradient-to-t from-red-600/100 to-red-400/80 pt-16 pb-24 gap-10 items-center laptop:items-start text-warm-white">
+      <div className="flex flex-col w-10/12 mx-auto laptop:w-8/12 gap-10 ">
         <h3 className=" text-2xl self-start">Aktuellt</h3>
         <div className="flex flex-col laptop:flex-row gap-16 laptop:gap-18">
           <div className="flex flex-col gap-6 laptop:w-1/2 laptop:flex-row">
@@ -21,7 +21,7 @@ export const Aktuellt = () => {
                 Aldgård, som också regisserar.
               </p>
               <p>Premiär: 13 juni 2025</p>
-              <a href="https://evarydberg.se/biljettbokning/" target="_blank">
+              <a href="https://evarydberg.se/" target="_blank">
                 <button className="cursor-pointer bg-gradient-to-t from-red-700/80 to-red-300/80  hover:bg-red/100 w-fit rounded-2xl p-2 px-3 text-warm-white text-xs hover:scale-105 self-center tablet:self-start">
                   Köp biljetter
                 </button>
--- src/components/Galleri.jsx
+++ src/components/Galleri.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react";
+import images from "../data/ImageData.json";
+import { SlArrowUp } from "react-icons/sl";
+import { SlArrowDown } from "react-icons/sl";
+
 export const Galleri = () => {
+  const [showAll, setShowAll] = useState(false);
+  const displayedImages = showAll ? images : images.slice(0, 4);
+
   return (
-    <section className="w-10/12 laptop:w-8/12 mx-auto py-20 flex flex-col gap-4">
-        <h3 className=" text-2xl self-start text-red">Tidigare projekt</h3>
+    <section className="w-10/12 laptop:w-8/12 mx-auto py-20 flex flex-col gap-10">
+      <h3 className=" text-2xl self-start text-red">Tidigare projekt</h3>
       <div className="flex flex-col laptop:flex-row gap-4">
         <iframe
           className="w-full h-full aspect-[6/4] rounded-xl"
@@ -25,6 +33,31 @@ export const Galleri = () => {
           allowfullscreen
         ></iframe>
       </div>
+      <ul className="grid grid-cols-4  gap-4 laptop:gap-6">
+        {displayedImages.map((img, index) => (
+          <li key={index}>
+            <img
+              src={img.image}
+              alt={img.alt}
+              className="aspect-[4/3] object-cover"
+            />
+          </li>
+        ))}
+      </ul>
+      <button
+        className="flex gap-2 items-center bg-gradient-to-t from-red-600/80 to-red-300/80  hover:bg-red/100 w-fit rounded-2xl p-2 px-3 text-warm-white text-sm hover:scale-105 self-center tablet:self-end mt-4"
+        onClick={() => setShowAll(!showAll)}
+      >
+        {showAll ? (
+          <>
+            Visa mindre <SlArrowUp />
+          </>
+        ) : (
+          <>
+            Visa fler <SlArrowDown />
+          </>
+        )}
+      </button>
     </section>
   );
 };
