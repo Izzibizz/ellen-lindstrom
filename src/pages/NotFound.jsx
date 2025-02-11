62fc233791044d4172b00f058fac01f2f7d3da29
izzibizz <contact@izabellind.com>
2025-02-11 08:12:36 +0100

    notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,12 @@
 
 
 export const NotFound = () => {
+
   return (
-    <section>
-      not found
+    <section className= {`flex flex-col items-center justify-center h-[70vh] font-round font-medium gap-10 text-red text-2xl laptop:text-3xl`}>
+      <img src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExOHp3cHoybnE5ODdyN3RwYzd0ZzR6YzlsMjZ0eGF2c3I2MG8wZm1xbSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oKIPaR7oa6Ri958He/giphy.gif" alt="404 image" className="object-cover aspect-square rounded-[50%]"/>
+     Page not found
     </section>
   )
 }
-
 
